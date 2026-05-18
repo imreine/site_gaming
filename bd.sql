bf26e6aa83f9cc033d1fc1289cd3b564a8ff3ff4
imreine <areineimmaculee@gmail.com>
2026-05-18 18:09:48 +0000

    Change ID column to use AUTO_INCREMENT

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@ CREATE DATABASE gaming_site;
 USE gaming_site;
 
 CREATE TABLE users (
-    id INT IDENTITY(1,1) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
     pseudo VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
